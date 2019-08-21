e1df0cd599f5b7292ee7de536f5d248ffd3b056d
gilangsuryalaga <gilangsuryalaga@gmail.com>
2019-08-21 09:31:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanfragment/Main2Activity.java
@@ -0,0 +1,58 @@
+package com.example.latihanfragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navView = findViewById(R.id.nav_menu);
+        navView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null ;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new DashboardFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new NotificationsFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+
+        private boolean loadFragment(Fragment fragment){
+            if (fragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                return true;
+            }
+            return false;
+        }
+    }
+
+
